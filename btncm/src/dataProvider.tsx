8d83bdc9558d57822a5ca9244108d91bd7f45f2f
龙打野 <18128966990@163.com>
2024-03-22 16:54:08 +0800

    Update dataProvider.tsx

--- btncm/src/dataProvider.tsx
+++ btncm/src/dataProvider.tsx
@@ -1,12 +1,6 @@
-import {
-    DataProvider,
-    fetchUtils,
-    HttpError,
-    withLifecycleCallbacks,
-} from 'react-admin';
+import { DataProvider, fetchUtils, withLifecycleCallbacks } from 'react-admin';
 import get from 'lodash/get';
 import simpleRestProvider from 'ra-data-simple-rest';
-import addUploadFeature from './addUploadFeature';
 import moment from 'moment';
 import querystring from 'querystring';
 
@@ -28,8 +22,6 @@ const tokenNotifyClient = (url, options: any = {}) =>
     });
 
 const withLifeCycleCallbackProvider = withLifecycleCallbacks(
-    // 要用时加上
-    // fakeRestProvider(data, true),
     simpleRestProvider('/api', tokenNotifyClient),
     [
         {
@@ -334,57 +326,6 @@ const addTagsSearchSupport = (dataProvider: DataProvider) => ({
         }
 
         return dataProvider.getList(resource, params);
-    },
-});
-
-const uploadCapableDataProvider = addUploadFeature(
-    addTagsSearchSupport(dataProvider)
-);
-
-const sometimesFailsDataProvider = new Proxy(uploadCapableDataProvider, {
-    get: (target, name) => (resource, params) => {
-        if (typeof name === 'symbol' || name === 'then') {
-            return;
-        }
-        // set session_ended=true in localStorage to trigger an API auth error
-        if (localStorage.getItem('session_ended')) {
-            const error = new Error('Session ended') as ResponseError;
-            error.status = 403;
-            return Promise.reject(error);
-        }
-        // add rejection by type or resource here for tests, e.g.
-        // if (name === 'delete' && resource === 'posts') {
-        //     return Promise.reject(new Error('deletion error'));
-        // }
-        if (
-            resource === 'posts' &&
-            params.data &&
-            params.data.title === 'f00bar'
-        ) {
-            return Promise.reject(
-                new HttpError('The form is invalid', 400, {
-                    errors: {
-                        title: 'this title cannot be used',
-                    },
-                })
-            );
-        }
-        return uploadCapableDataProvider[name](resource, params);
-    },
-});
-
-const delayedDataProvider = new Proxy(sometimesFailsDataProvider, {
-    get: (target, name) => (resource, params) => {
-        if (typeof name === 'symbol' || name === 'then') {
-            return;
-        }
-        return new Promise(resolve =>
-            setTimeout(
-                () =>
-                    resolve(sometimesFailsDataProvider[name](resource, params)),
-                300
-            )
-        );
     },
 });
 
